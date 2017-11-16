1b9a55708d2030791f139fd9045d97c76715ff2c
saulopineda <saulopineda@gmail.com>
2017-11-15 19:49:42 -0500

    Bases al ejecutar
    Ejecutado

--- presentacion/proyectomejorado/src/proyecto/Desbloqueo.java
+++ presentacion/proyectomejorado/src/proyecto/Desbloqueo.java
@@ -176,6 +176,9 @@ public class Desbloqueo extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Desbloqueo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
